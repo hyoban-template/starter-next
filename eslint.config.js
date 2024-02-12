3f87e0567391742d03c08fe79c2c4411644ac451
Stephen Zhou <hi@hyoban.cc>
2024-02-13 00:22:21 +0800

    chore: use gitignore

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,23 @@
+import gitignore from 'eslint-config-flat-gitignore'
 import js from '@eslint/js'
 import stylistic from '@stylistic/eslint-plugin'
 
+const GLOB_SRC = '**/*.?([cm])[jt]s?(x)'
+
 export default [
-  js.configs.recommended,
-  stylistic.configs['recommended-flat'],
+  gitignore({
+    files: [
+      '.gitignore',
+      '.eslintignore',
+    ],
+    strict: false,
+  }),
+  {
+    files: [GLOB_SRC],
+    rules: js.configs.recommended.rules,
+  },
+  {
+    ...stylistic.configs['recommended-flat'],
+    files: [GLOB_SRC],
+  },
 ]
