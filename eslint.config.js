f0ba16335d12475e1ded8ab16a5a313b394e09ac
Stephen Zhou <hi@hyoban.cc>
2024-02-13 00:48:58 +0800

    chore: add antfu

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
 // Install dependencies
-// ni -D eslint eslint-config-flat-gitignore @eslint/js @stylistic/eslint-plugin eslint-plugin-unicorn eslint-plugin-simple-import-sort
+// ni -D eslint eslint-config-flat-gitignore @eslint/js @stylistic/eslint-plugin eslint-plugin-unicorn eslint-plugin-simple-import-sort eslint-plugin-antfu
 
 import js from '@eslint/js'
 import stylistic from '@stylistic/eslint-plugin'
 import gitignore from 'eslint-config-flat-gitignore'
+import eslintPluginAntfu from 'eslint-plugin-antfu'
 import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import eslintPluginUnicorn from 'eslint-plugin-unicorn'
 
@@ -35,8 +36,28 @@ export default createFlatConfig([
     ],
     strict: false,
   }),
-  js.configs.recommended,
+  {
+    rules: {
+      ...js.configs.recommended.rules,
+      'prefer-template': 'error',
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+    },
+  },
   stylistic.configs['recommended-flat'],
+  {
+    plugins: {
+      antfu: eslintPluginAntfu,
+    },
+    rules: {
+      'antfu/consistent-list-newline': 'error',
+      'antfu/if-newline': 'error',
+      'antfu/top-level-function': 'error',
+
+      'antfu/import-dedupe': 'error',
+      'antfu/no-import-dist': 'error',
+      'antfu/no-import-node-modules-by-path': 'error',
+    },
+  },
   eslintPluginUnicorn.configs['flat/recommended'],
   {
     plugins: {
