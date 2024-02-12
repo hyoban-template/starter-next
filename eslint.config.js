c0990c6900c7c6f0092bc6ba38e6b54ed74cf87c
Stephen Zhou <hi@hyoban.cc>
2024-02-13 00:29:45 +0800

    chore: add unicorn import-sort

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
 // Install dependencies
-// ni -D eslint eslint-config-flat-gitignore @eslint/js @stylistic/eslint-plugin
+// ni -D eslint eslint-config-flat-gitignore @eslint/js @stylistic/eslint-plugin eslint-plugin-unicorn eslint-plugin-simple-import-sort
 
-import gitignore from 'eslint-config-flat-gitignore'
 import js from '@eslint/js'
 import stylistic from '@stylistic/eslint-plugin'
+import gitignore from 'eslint-config-flat-gitignore'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import eslintPluginUnicorn from 'eslint-plugin-unicorn'
 
 const GLOB_SRC = '**/*.?([cm])[jt]s?(x)'
 
@@ -19,8 +21,15 @@ export default [
     files: [GLOB_SRC],
     rules: js.configs.recommended.rules,
   },
+  stylistic.configs['recommended-flat'],
+  eslintPluginUnicorn.configs['flat/recommended'],
   {
-    ...stylistic.configs['recommended-flat'],
-    files: [GLOB_SRC],
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
   },
 ]
