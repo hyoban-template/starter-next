05cc3a03a5cd55ed96d614d3cc218e066b75ec96
Stephen Zhou <hi@hyoban.cc>
2024-02-13 22:51:05 +0800

    chore: update

---
+++ stub.d.ts
@@ -0,0 +1,5 @@
+declare module '@next/eslint-plugin-next'
+declare module 'eslint-plugin-import'
+declare module 'eslint-plugin-react-hooks'
+declare module 'eslint-plugin-simple-import-sort'
+declare module 'eslint-plugin-unicorn'
