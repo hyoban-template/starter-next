9a76f68c84ebd0259e35178d997219353cbf1b23
Stephen Zhou <hi@hyoban.cc>
2024-03-11 11:25:25 +0800

    chore: update deps

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
       <body className="h-full">
         {
           // eslint-disable-next-line @eslint-react/dom/no-dangerously-set-innerhtml
-          <script dangerouslySetInnerHTML={{ __html: `!function(){var e=window.matchMedia&&window.matchMedia("(prefers-color-scheme: dark)").matches,t=localStorage.getItem("use-dark")||'"system"';('"dark"'===t||e&&'"light"'!==t)&&document.documentElement.classList.toggle("dark",!0)}();` }}></script>
+          <script dangerouslySetInnerHTML={{ __html: `!function(){var e=window.matchMedia&&window.matchMedia("(prefers-color-scheme: dark)").matches,t=localStorage.getItem("use-dark")||'"system"';('"dark"'===t||e&&'"light"'!==t)&&document.documentElement.classList.toggle("dark",!0)}();` }} />
         }
         <Provider>
           {children}
