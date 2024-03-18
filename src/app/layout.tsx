6568ffffbad9af760ce3d064274a9beabba1b11c
Stephen Zhou <hi@hyoban.cc>
2024-03-18 11:04:06 +0800

    chore: update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode,
 }) {
   return (
     <html lang="zh-Hans" suppressHydrationWarning className="h-full">
